f2e6b3d65a35f2444375da4691b5b17da8aef03e
lbanasze <laura.banaszewski@uvm.edu>
2020-09-04 10:46:10 -0500

    Step 48

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,10 +1,14 @@
 var controllerOptions = {};
 var i = 0;
+var x = (window.innerWidth)/2;
+var y = (window.innerHeight)/2;
 
 Leap.loop(controllerOptions, function(frame)
 	{
 		console.log(i);
 		i += 1;
+
+		circle(x, y, 100)
 	}
 );
 
