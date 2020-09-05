784544ed9d77122f5449746b41b6bc248450552c
lbanasze <laura.banaszewski@uvm.edu>
2020-09-05 09:42:37 -0500

    Step 49

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -2,13 +2,17 @@ var controllerOptions = {};
 var i = 0;
 var x = (window.innerWidth)/2;
 var y = (window.innerHeight)/2;
-
+var ran;
++
 Leap.loop(controllerOptions, function(frame)
 	{
 		console.log(i);
 		i += 1;
 
-		circle(x, y, 100)
+		//Math.random() * (max - min) + min;
+		ran = Math.random() * 2 - 1;
+
+		circle(x+ran, y, 100);
 	}
 );
 
