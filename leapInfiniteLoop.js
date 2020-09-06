44f614463e01e5342ff616fafb4892d3763bfb2d
lbanasze <laura.banaszewski@uvm.edu>
2020-09-06 15:43:48 -0500

    Step 64

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -6,26 +6,43 @@ var ran;
 var ran2;
 var hand;
 var fingers;
+var finger; 
 
 Leap.loop(controllerOptions, function(frame)
 	{
-		//clear();
-
-		//console.log(i);
-		//i+=1;
-
-		//ran = Math.floor(Math.random() * 2 - 1);
-		//ran2 = Math.floor(Math.random() * 2 - 1);
-
-		//circle((x+ran), (y+ran2), 100);
-	
-		if(frame.hands.length == 1)
-		{
-			hand = (frame.hands[0]);
-			fingers = hand.fingers;
-			
-			console.log(fingers);
-		}
+		HandleFrame(frame);
 	}
 );
 
+function HandleFrame(frame){
+	//clear();
+
+        //console.log(i);
+        //i+=1;
+
+       	//ran = Math.floor(Math.random() * 2 - 1);
+        //ran2 = Math.floor(Math.random() * 2 - 1);
+
+        //circle((x+ran), (y+ran2), 100);
+
+        if(frame.hands.length == 1)
+        	{
+			hand = frame.hands[0];
+			HandleHand(hand)
+                }
+
+}
+
+function HandleHand(hand){
+        fingers = hand.fingers;
+
+        for (i = 0; i < fingers.length; i++) {
+		HandleFinger(fingers[i]); 
+      	}
+}
+
+function HandleFinger(finger){
+	if(finger.type == 1){
+		console.log(finger.type);
+	}
+}
