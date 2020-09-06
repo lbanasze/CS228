7cc1bdccd33a94daa7e92774223ac3edd267dd29
lbanasze <laura.banaszewski@uvm.edu>
2020-09-06 14:18:09 -0500

    Step 54

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -2,20 +2,22 @@ var controllerOptions = {};
 var i = 0;
 var x = (window.innerWidth)/2;
 var y = (window.innerHeight)/2;
-var ran, ran2;
+var ran;
+var ran2;
 
 Leap.loop(controllerOptions, function(frame)
 	{
-		clear();
+		//clear();
 
-		console.log(i);
-		i+=1;
+		//console.log(i);
+		//i+=1;
 
-		//Math.random() * (max - min) + min;
-		ran = Math.random() * 2 - 1;
-		ran2 = Math.random() * 2 - 1;
+		//ran = Math.floor(Math.random() * 2 - 1);
+		//ran2 = Math.floor(Math.random() * 2 - 1);
 
-		circle(x+ran, y+ran2, 100);
+		//circle((x+ran), (y+ran2), 100);
+		
+		console.log(frame.hands);
 	}
 );
 
