38c77049fa7ace550a37b97a78f07dfa8b0ed332
lbanasze <laura.banaszewski@uvm.edu>
2020-10-26 14:06:09 -0500

    Del 07 Step 14

--- PredictGestures.js
+++ PredictGestures.js
@@ -5,21 +5,18 @@ var predictedLabel;
 var oneFrameOfData = nj.zeros([5,4,6]);
 var n = 0;
 var m = 1;
-var d = 3;
+var d = 0;
 
 const knnClassifier = ml5.KNNClassifier();
 
 function HandleFrame(frame){
 
 	var InteractionBox = frame.interactionBox;
-        
+       
 	if(frame.hands.length >= 1)
         	{
 			hand = frame.hands[0];
 			HandleHand(hand, InteractionBox);
-			CenterXData();
-			CenterYData();
-			CenterZData();
 			Test();
                 }
 
@@ -70,14 +67,150 @@ function HandleBone(bone, fingerIndex, InteractionBox){
 
 function Train(){
 	for (i = 0; i < train2.shape[3]; i ++){
+		
+		features = train0Allison.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 0);
+
+		features = train0Bongard.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 0);
+		
+		features = train0Davis.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 0);
+		
+		features = train1Allison.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 1);
+
+		features = train1Bongard.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 1);
+		
+		features = train1Davis.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 1);
+		
+		features = train1Jimmo.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 1);
+		
+		features = train1Li.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 1);
+		
+		features = train1McLaughlin.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 1);
+		
+		features = train1Rice.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 1);
+		
+		features = train1Riofrio.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 1);
+		
 		features = train2.pick(null, null, null, i);
 		features = features.reshape(1, 120);
 		knnClassifier.addExample(features.tolist(), 2);
+	
+		features = train2Bongard.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 2);
+	
+		features = train2Jimmo.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 2);
+	
 		features = train3.pick(null,null,null, i);
 		features = features.reshape(1, 120);
 		knnClassifier.addExample(features.tolist(), 3);
+		
+		features = train3Beattie.pick(null,null,null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 3);
+		
+		features = train3Bongard.pick(null,null,null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 3);
+		
+		features = train4Beattie.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 4);
+		
+		features = train4Bongard.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 4);
+		
+		features = train4Faucher.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 4);
+		
+		features = train5Blewett.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 5);
+		
+		features = train5Bongard.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 5);
+		
+		features = train5Faucher.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 5);
+		
+		features = train6Blewett.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 6);
+		
+		features = train6Bongard.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 6);
+		
+		features = train6Fisher.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 6);
+		
+		features = train7Bongard.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 7);
+		
+		features = train7Fisher.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 7);
+		
+		features = train7Manian.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 7);
+		
+		features = train8Bongard.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 8);
+		
+		features = train8Goldman.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 8);
+		
+		features = train8Matthews.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 8);
+		
+		features = train9Bongard.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 9);
+	 	
+		features = train9Goldman.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 9);
+	 	
+		features = train9He.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 9);
+	 	
 	}
 }
+
 
 function CenterXData(){
 	var xValues = oneFrameOfData.slice([], [], [0,6,3]);
@@ -91,12 +224,14 @@ function CenterXData(){
 			currentX = oneFrameOfData.get(i, j, 0);
 			shiftedX = currentX + horizontalShift;
 			oneFrameOfData.set(i, j, 0, shiftedX);
-			
 			currentX = oneFrameOfData.get(i, j, 1);
 			shiftedX = currentX + horizontalShift;
-			oneFrameOfData.set(i, j, 1, shiftedX);
+			oneFrameOfData.set(i, j, 3, shiftedX);
+//			console.log(shiftedX);
 		}
 	}
+
+//	console.log(oneFrameOfData.toString());
 
 	currentMean = xValues.mean();
 }
@@ -105,18 +240,19 @@ function CenterYData(){
 	var yValues = oneFrameOfData.slice([], [], [1,6,3]);
 	var currentMean = yValues.mean();
 	var horizontalShift = 0.5 - currentMean;
-
 	var currentY;
+//	console.log(currentY);
 	var shiftedY;
 	for(i = 0; i < 5; i++){
 		for(j = 0; j < 4; j++){
-			currentY = oneFrameOfData.get(i, j, 2);
+			currentY = oneFrameOfData.get(i, j, 1);
 			shiftedY = currentY + horizontalShift;
-			oneFrameOfData.set(i, j, 2, shiftedY);
+//			console.log(shiftedY);
+			oneFrameOfData.set(i, j, 1, shiftedY);
 			
-			currentY = oneFrameOfData.get(i, j, 3);
+			currentY = oneFrameOfData.get(i, j, 4);
 			shiftedY = currentY + horizontalShift;
-			oneFrameOfData.set(i, j, 3, shiftedY);
+			oneFrameOfData.set(i, j, 4, shiftedY);
 		}
 	}
 
@@ -132,13 +268,13 @@ function CenterZData(){
 	var shiftedZ;
 	for(i = 0; i < 5; i++){
 		for(j = 0; j < 4; j++){
-			currentZ = oneFrameOfData.get(i, j, 4);
+			currentZ = oneFrameOfData.get(i, j, 2);
 			shiftedZ = currentZ + horizontalShift;
-			oneFrameOfData.set(i, j, 4, shiftedZ);
+			oneFrameOfData.set(i, j, 2, shiftedZ);
 			
-			currentZ = oneFrameOfData.get(i, j, 4);
+			currentZ = oneFrameOfData.get(i, j, 5);
 			shiftedZ = currentZ + horizontalShift;
-			oneFrameOfData.set(i, j, 4, shiftedZ);
+			oneFrameOfData.set(i, j, 5, shiftedZ);
 		}
 	}
 
@@ -149,6 +285,9 @@ function Test(){
 
 	for(i = 0; i < oneFrameOfData.shape[2]; i++){	
 		var currentSample = oneFrameOfData.pick(null, null, null, i);
+		CenterXData();
+		CenterYData();
+		CenterZData();
 		currentSample = currentSample.reshape(1,120);
 		predictedLabel = knnClassifier.classify(currentSample.tolist(), GotResults);i
 	}
@@ -159,7 +298,7 @@ function GotResults(err, result){
 	n++;
 	c = result.label;
 	m = ((n-1)*m + (c == d))/n
-	console.log(n, m, c);
+	console.log(c);
 }
 
 Leap.loop(controllerOptions, function(frame){
