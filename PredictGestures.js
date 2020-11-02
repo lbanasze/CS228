eedb71e057b7b46a5de635fe8c96ed6074e8ef83
lbanasze <laura.banaszewski@uvm.edu>
2020-11-02 11:38:26 -0600

    Del 08 Step 16

--- PredictGestures.js
+++ PredictGestures.js
@@ -6,6 +6,7 @@ var oneFrameOfData = nj.zeros([5,4,6]);
 var n = 0;
 var m = 1;
 var d = 0;
+var programState = 0;
 
 const knnClassifier = ml5.KNNClassifier();
 
@@ -17,9 +18,14 @@ function HandleFrame(frame){
         	{
 			hand = frame.hands[0];
 			HandleHand(hand, InteractionBox);
-			Test();
+	//		Test();
                 }
 
+}
+
+function SignIn(){
+	username = document.getElementById('username').value;
+	console.log(username);
 }
 
 function HandleHand(hand, InteractionBox)
@@ -51,10 +57,10 @@ function HandleBone(bone, fingerIndex, InteractionBox){
 	oneFrameOfData.set(fingerIndex, bone.type, 4, normalizedNextJoint[1]);
 	oneFrameOfData.set(fingerIndex, bone.type, 5, normalizedNextJoint[2]);
 
-	x1 = window.innerWidth * normalizedPrevJoint[0];
-	x2 = window.innerWidth * normalizedNextJoint[0];
-	y1 = window.innerHeight * (1 - normalizedPrevJoint[1]);
-	y2 = window.innerHeight * (1 - normalizedNextJoint[1]);
+	x1 = window.innerWidth/2 * normalizedPrevJoint[0];
+	x2 = window.innerWidth/2 * normalizedNextJoint[0];
+	y1 = window.innerHeight/2 * (1 - normalizedPrevJoint[1]);
+	y2 = window.innerHeight/2 * (1 - normalizedNextJoint[1]);
 	z1 = normalizedPrevJoint[2];
 	z2 = normalizedNextJoint[2];
 
@@ -68,27 +74,39 @@ function HandleBone(bone, fingerIndex, InteractionBox){
 function Train(){
 	for (i = 0; i < train2.shape[3]; i ++){
 		
+		features = train0.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 0);
+
+		features = train1.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 1);
+
+		features = train2.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 2);
+
+		features = train3.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 3);
+
+		features = train4.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 4);
+
+		features = train5.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 5);
+
+/*		features = train0Bongard.pick(null, null, null, i);
+		features = features.reshape(1, 120);
+		knnClassifier.addExample(features.tolist(), 0);
+
 		features = train0Allison.pick(null, null, null, i);
 		features = features.reshape(1, 120);
 		knnClassifier.addExample(features.tolist(), 0);
 
-		features = train0Bongard.pick(null, null, null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 0);
-		
-		features = train0Davis.pick(null, null, null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 0);
-		
-		features = train1Allison.pick(null, null, null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 1);
-
 		features = train1Bongard.pick(null, null, null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 1);
-		
-		features = train1Davis.pick(null, null, null, i);
 		features = features.reshape(1, 120);
 		knnClassifier.addExample(features.tolist(), 1);
 		
@@ -96,26 +114,6 @@ function Train(){
 		features = features.reshape(1, 120);
 		knnClassifier.addExample(features.tolist(), 1);
 		
-		features = train1Li.pick(null, null, null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 1);
-		
-		features = train1McLaughlin.pick(null, null, null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 1);
-		
-		features = train1Rice.pick(null, null, null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 1);
-		
-		features = train1Riofrio.pick(null, null, null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 1);
-		
-		features = train2.pick(null, null, null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 2);
-	
 		features = train2Bongard.pick(null, null, null, i);
 		features = features.reshape(1, 120);
 		knnClassifier.addExample(features.tolist(), 2);
@@ -123,19 +121,11 @@ function Train(){
 		features = train2Jimmo.pick(null, null, null, i);
 		features = features.reshape(1, 120);
 		knnClassifier.addExample(features.tolist(), 2);
-	
-		features = train3.pick(null,null,null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 3);
-		
-		features = train3Beattie.pick(null,null,null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 3);
-		
+
 		features = train3Bongard.pick(null,null,null, i);
 		features = features.reshape(1, 120);
 		knnClassifier.addExample(features.tolist(), 3);
-		
+	
 		features = train4Beattie.pick(null, null, null, i);
 		features = features.reshape(1, 120);
 		knnClassifier.addExample(features.tolist(), 4);
@@ -144,19 +134,7 @@ function Train(){
 		features = features.reshape(1, 120);
 		knnClassifier.addExample(features.tolist(), 4);
 		
-		features = train4Faucher.pick(null, null, null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 4);
-		
-		features = train5Blewett.pick(null, null, null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 5);
-		
 		features = train5Bongard.pick(null, null, null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 5);
-		
-		features = train5Faucher.pick(null, null, null, i);
 		features = features.reshape(1, 120);
 		knnClassifier.addExample(features.tolist(), 5);
 		
@@ -180,10 +158,6 @@ function Train(){
 		features = features.reshape(1, 120);
 		knnClassifier.addExample(features.tolist(), 7);
 		
-		features = train7Manian.pick(null, null, null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 7);
-		
 		features = train8Bongard.pick(null, null, null, i);
 		features = features.reshape(1, 120);
 		knnClassifier.addExample(features.tolist(), 8);
@@ -203,11 +177,7 @@ function Train(){
 		features = train9Goldman.pick(null, null, null, i);
 		features = features.reshape(1, 120);
 		knnClassifier.addExample(features.tolist(), 9);
-	 	
-		features = train9He.pick(null, null, null, i);
-		features = features.reshape(1, 120);
-		knnClassifier.addExample(features.tolist(), 9);
-	 	
+*/
 	}
 }
 
@@ -227,11 +197,9 @@ function CenterXData(){
 			currentX = oneFrameOfData.get(i, j, 1);
 			shiftedX = currentX + horizontalShift;
 			oneFrameOfData.set(i, j, 3, shiftedX);
-//			console.log(shiftedX);
 		}
 	}
 
-//	console.log(oneFrameOfData.toString());
 
 	currentMean = xValues.mean();
 }
@@ -241,7 +209,6 @@ function CenterYData(){
 	var currentMean = yValues.mean();
 	var horizontalShift = 0.5 - currentMean;
 	var currentY;
-//	console.log(currentY);
 	var shiftedY;
 	for(i = 0; i < 5; i++){
 		for(j = 0; j < 4; j++){
@@ -297,18 +264,187 @@ function Test(){
 function GotResults(err, result){
 	n++;
 	c = result.label;
-	m = ((n-1)*m + (c == d))/n
+	m = ((n-1)*m + (c == d))/n;
 	console.log(c);
+}
+
+function DrawArrowLeft(){
+	image(imgLeft, window.innerWidth/2, 0, window.innerWidth/2, window.innerHeight/2);
+}
+
+function DrawArrowRight(){
+	image(imgRight, window.innerWidth/2, 0, window.innerWidth/2, window.innerHeight/2);
+}
+
+function DrawArrowUp(){
+	image(imgUp, window.innerWidth/2, 0, window.innerWidth/2, window.innerHeight/2);
+}
+
+function DrawArrowDown(){
+	image(imgDown, window.innerWidth/2, 0, window.innerWidth/2, window.innerHeight/2);
+}
+
+function DrawArrowToward(){
+	image(imgToward, window.innerWidth/2, 0, window.innerWidth/2, window.innerHeight/2);
+}
+
+function DrawArrowAway(){
+	image(imgAway, window.innerWidth/2, 0, window.innerWidth/2, window.innerHeight/2);
+}
+
+function HandTooFarLeft(){
+	var xValues = oneFrameOfData.slice([], [], [0,6,3]);
+	var currentMean = xValues.mean();
+
+	if(currentMean < 0.25){
+		return true;
+	}
+	
+	return false;
+}
+
+function HandTooFarRight(){
+	var xValues = oneFrameOfData.slice([], [], [0,6,3]);
+	var currentMean = xValues.mean();
+
+	if(currentMean > 0.75){
+		return true;
+	}
+
+	return false;
+}
+
+function HandTooHigh(){
+	var yValues = oneFrameOfData.slice([], [], [1,6,3]);
+	var currentMean = yValues.mean();
+	
+	if(currentMean > 0.75){
+		return true;
+	}
+
+	return false;
+}
+
+function HandTooLow(){
+	var yValues = oneFrameOfData.slice([], [], [1,6,3]);
+	var currentMean = yValues.mean();
+	console.log(currentMean);	
+	if(currentMean <  0.25){
+		return true;
+	}
+
+	return false;
+}
+
+function HandTooFar(){
+	var zValues = oneFrameOfData.slice([], [], [2,6,3]);
+	var currentMean = zValues.mean();
+
+	if(currentMean <  0.25){
+		return true;
+	}
+
+	return false;
+}
+
+function HandTooClose(){
+	var zValues = oneFrameOfData.slice([], [], [2,6,3]);
+	var currentMean = zValues.mean();
+	if(currentMean > 0.75){
+		return true;
+	}
+
+	return false;
+}
+
+function HandIsUncentered(){
+	if(HandTooFarRight() || HandTooFarLeft() || HandTooHigh() || HandTooLow() || HandTooFar() || HandTooClose()){
+		return true;
+	}
+
+	return false;
+}
+
+function DetermineState(frame){
+	if(frame.hands.length == 0){
+		programState = 0;
+	}
+
+	else if(HandIsUncentered()){
+		programState = 1;
+	}
+
+	else{
+		programState = 2; 
+	}
+}
+
+function TrainKnn(){
+	if (!trainingCompleted){
+		Train();
+		trainingCompleted = true;
+	}
+}
+
+function DrawImage(){
+	image(img, 0, 0, window.innerWidth/2, window.innerHeight/2);
+}
+
+function HandleState0(frame){
+//	TrainKnn();
+	DrawImage();
+}
+
+function HandleState1(frame){
+
+	HandleFrame(frame);
+	
+	if(HandTooFarLeft()){
+		DrawArrowRight();
+	}
+
+	else if(HandTooFarRight()){
+		DrawArrowLeft();
+	}
+
+	if(HandTooHigh()){
+		DrawArrowDown();
+	}
+
+	if(HandTooLow()){
+		DrawArrowUp();
+	}
+
+	if(HandTooFar()){
+		DrawArrowToward();
+	}
+	
+	else if(HandTooClose()){
+		DrawArrowAway();
+	}
+
+
+}
+
+function HandleState2(frame){
+	HandleFrame(frame);
 }
 
 Leap.loop(controllerOptions, function(frame){
 	
 	clear();
 
-	if (!trainingCompleted){
-		Train();
-		trainingCompleted = true;
+	DetermineState(frame);
+	if (programState == 0){
+		HandleState0(frame);
 	}
 
-	HandleFrame(frame);
+	else if (programState == 1){
+		HandleState1(frame);
+	}
+
+	else{
+		HandleState2(frame);
+	}
+
 });
--- prepareToDraw.js
+++ prepareToDraw.js
@@ -1,3 +1,12 @@
+var img;
+
 function setup(){
 	createCanvas(window.innerWidth,window.innerHeight);
+	img = loadImage('https://i.imgur.com/pUTGk7A.jpg');
+	imgAway = loadImage('https://i.imgur.com/SnAP1gz.jpg');
+	imgToward = loadImage('https://i.imgur.com/4J0RKB8.jpg');
+	imgDown = loadImage('https://i.imgur.com/5EYxjGk.jpg');
+	imgUp = loadImage('https://i.imgur.com/5PL1Uiy.jpg');
+	imgRight = loadImage('https://i.imgur.com/MP1KBXW.jpg');
+	imgLeft = loadImage('https://i.imgur.com/lnHuHPl.jpg');
 }
