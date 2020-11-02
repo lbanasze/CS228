d0559be6e8b9c2e5ac8e8b89099394a0d7626c7d
lbanasze <laura.banaszewski@uvm.edu>
2020-11-02 12:45:46 -0600

    Del 08 Step 51

--- PredictGestures.js
+++ PredictGestures.js
@@ -35,14 +35,33 @@ function IsNewUser(username, list){
 	return usernameFound == false; 
 }
 
+function CreateNewUser(username, list){
+
+	item = document.createElement('li');
+	item.id = String(username) + "_name";
+	item.innerHTML = String(username);
+	list.appendChild(item);
+}
+
+function CreateSignInItem(username, list){
+	item = document.createElement('li');
+	item.id = String(username) + "_signins";
+	item.innerHTML = 1; 
+	list.appendChild(item); 
+}
+
 function SignIn(){
 	username = document.getElementById('username').value;
 	console.log(username);
 	var list = document.getElementById('users');
 	if (IsNewUser(username, list)){
-		var item = document.createElement('li');
-		item.innerHTML = String(username);
-		list.appendChild(item);
+		CreateNewUser(username, list);
+		CreateSignInItem(username, list);
+	}
+	else{
+		ID = String(username) + "_signins";
+		listItem = document.getElementById(ID);
+		listItem.innerHTML = parseInt(listItem.innerHTML) + 1; 
 	}
 	console.log(list.innerHTML);
 	return false;
