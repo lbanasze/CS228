38529426efc39e44307fdf32a91e24dde549df85
lbanasze <58741108+lbanasze@users.noreply.github.com>
2020-11-15 20:25:46 -0600

    added the rest of the images
    album: https://imgur.com/a/ljBQqC6

--- prepareToDraw.js
+++ prepareToDraw.js
@@ -9,6 +9,14 @@ function setup(){
 	imgUp = loadImage('https://i.imgur.com/5PL1Uiy.jpg');
 	imgRight = loadImage('https://i.imgur.com/MP1KBXW.jpg');
 	imgLeft = loadImage('https://i.imgur.com/lnHuHPl.jpg');
-	img2 = loadImage('https://i.imgur.com/BM0n4JJ.jpg');
-	img3 = loadImage('https://i.imgur.com/DUBtzhY.jpg');
+	img0 = loadImage('https://i.imgur.com/CEyUqwd.jpg');
+	img1 = loadImage('https://i.imgur.com/Bm6uULe.jpg');
+	img2 = loadImage('https://i.imgur.com/jGw0wOi.jpg');
+	img3 = loadImage('https://i.imgur.com/GJfIgI3.jpg');
+	img4 = loadImage('https://i.imgur.com/kDxQj1t.jpg');
+	img5 = loadImage('https://i.imgur.com/cuqE8F7.jpg');
+	img6 = loadImage('https://i.imgur.com/PDtk8JQ.jpg');
+	img7 = loadImage('https://i.imgur.com/L733mpP.jpg');
+	img8 = loadImage('https://i.imgur.com/YLdigYp.jpg');
+	img9 = loadImage('https://i.imgur.com/mLnKogC.jpg');
 }
